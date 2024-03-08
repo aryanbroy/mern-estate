d42ef1e058b9a846d9da5bd51fc3c68345a4c816
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-03-09 00:16:01 +0530

    add routes, controllers and middlewares

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.DB_URI)
@@ -10,7 +14,22 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log(`Server running at server 3000`);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
